ccc20920fe3a3b6efb8876b30f05a4a104781247
Rita Mariquitos <59831623+armarik@users.noreply.github.com>
2023-07-12 15:29:54 +0000

    Add cat model

--- fantastic_train/supercats/models.py
+++ fantastic_train/supercats/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    CAT_SIZES = [
+        ("S", "Illegally smol"),
+        ("M", "Medium"),
+        ("C", "Chonk"),
+    ]    
+    name = models.CharField(max_length=30)
+    size = models.CharField(max_length=1, choices=CAT_SIZES)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
