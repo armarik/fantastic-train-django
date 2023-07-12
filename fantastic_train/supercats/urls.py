77e33a0386261214025c18063b105c4026f92566
Rita Mariquitos <59831623+armarik@users.noreply.github.com>
2023-07-12 15:30:23 +0000

    Update views

--- fantastic_train/supercats/urls.py
+++ fantastic_train/supercats/urls.py
@@ -3,4 +3,6 @@ from supercats import views
 
 urlpatterns = [
     path('', views.hello_cat, name='hello_cat'),
+    path("<int:cat_id>/results/", views.results, name="results"),
+    path("<int:cat_id>/vote/", views.vote, name="vote"),
 ]
--- fantastic_train/supercats/views.py
+++ fantastic_train/supercats/views.py
@@ -1,6 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from supercats.models import Cat
 
 # Create your views here.
 
 def hello_cat(request):
-    return render(request, 'hello_cat.html', {})
+    cat_list = Cat.objects.order_by("-created")[:5]
+
+    context = {
+        "cat_list": cat_list,
+    }
+    return render(request, "hello_cat.html", context)
+
+
+def results(request, cat_id: int) -> HttpResponse:
+    response = "You're looking at the results of cat %s."
+    return HttpResponse(response % cat_id)
+
+
+def vote(request, cat_id):
+    return HttpResponse("You're voting on cat %s." % cat_id)
