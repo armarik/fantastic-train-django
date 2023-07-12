264d081172f65d1f26760eeff1961e956fca2bef
Rita Mariquitos <59831623+armarik@users.noreply.github.com>
2023-07-12 15:30:11 +0000

    Add templates

--- fantastic_train/fantastic_train/settings.py
+++ fantastic_train/fantastic_train/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "fantastic_train.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["supercats/templates/"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
